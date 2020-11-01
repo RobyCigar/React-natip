6c1ca55ddbbb338a6124e925bdd05b967133970c
Rabih <rabihutomo11@gmail.com>
2020-11-01 19:30:06 +0700

    update

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/logo.png';
 
-export default function App() {
+function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    	<Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
+      <Text style={styles.instructions}>Tap to share a photo from your phone to your friends phone</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+	
+	logo: {
+		 width: 305, 
+		 height: 160 ,
+		 marginBottom: 10
+	},
+	
+	instructions: {
+		color:'#888', 
+		fontSize: 19 ,
+		marginHorizontal: 15,
+	},
+	
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+export default App;
